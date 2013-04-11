e23dd457701569f39b46585485a087a3ad40eb43
Patrick Landis <pal25@case.edu>
2013-04-11 14:35:43 -0400

    Started checkers

--- src/python/checkers.py
+++ src/python/checkers.py
@@ -1,41 +1,88 @@
-import subprocess, select
+import subprocess, select, os.path
 import pygame
-from pygame.locals import QUIT, MOUSEBUTTONUP, MOUSEBUTTONDOWN
 
 spim = subprocess.Popen(['spim', '-file', 'test.s'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-def setup_pygame(width=640, height=480):
-    pygame.init()
+class GameBoard():
+    def __init__(self, width=640, height=480):
+        self.size = (width, height)
+        self.screen = pygame.display.set_mode(self.size, pygame.RESIZABLE)
+        pygame.display.set_caption("Checkers in MIPS")
+        
+        colorbg = pygame.Surface(self.size).convert()
+        colorbg.fill((255, 255, 255))
+        self.screen.blit(colorbg, (0, 0))
+        self.background = self.load_image("background.png")
 
-    size = (width, height)
-    screen = pygame.display.set_mode(size)
-    pygame.display.set_caption('Checkers in MIPS')
+    def load_image(self, filename):
+        filename = os.path.join('images', filename)
 
-    background = pygame.Surface(screen.get_size())
-    background = background.convert()
-    background.fill((255, 255, 255))
+        try:
+            image = pygame.image.load(filename)
+        except pygame.error, errmsg:
+            print 'Cannot load background image: ', filename
+            raise SystemError, errmsg
 
-    return (screen, background)
+        return image
 
-def update_number(game, newval):
-    screen = game[0]
-    background = game[1]
+    def draw_board(self):
+        self.background = pygame.transform.scale(self.background, self.size)
 
-    background.fill((255, 255, 255))
+        colorbg = pygame.Surface(self.size).convert()
+        colorbg.fill((255, 255, 255))
 
+        self.screen.blit(colorbg, (0, 0))
+        self.screen.blit(self.background, self.background.get_rect())
+        pygame.display.update()
+
+class CheckerPiece(pygame.sprite.Sprite):
+    def __init__(self, player=1):
+        pygame.sprint.Sprint.__init__(self)
+        screen = pygame.display.get_surface()
+        
+        if player == 1:
+            self.image, self.rect = self.load_image('black_piece.bmp')
+        else:
+            self.image, self.rect = self.load_image('red_piece.bmp')
+        
+        self.player = player
+        self.piece_type = "pawn"
+
+        self.rect.centerx = centerx
+        self.rect.centery = centery
+        self.area = pygame.display.get_surface()
+
+    def load_image(self, filename):
+        filename = os.path.join('images', filename)
+
+        try:
+            image = pygame.image.load(filename)
+            image.set_colorkey(brown)
+        except pygame.error, errmsg:
+            print 'Cannot load background image: ', filename
+            raise SystemError, errmsg
+
+    def update(self):
+        newpos = move(self.rect)
+        pass
+        
+    def move(self):        
+        pass
+
+def update_number(board, newval):
     font = pygame.font.Font(None, 36)
     text = font.render(str(newval), 1, (10, 10, 10))
     textpos = text.get_rect()
-    textpos.centerx = background.get_rect().centerx
-    textpos.centery = background.get_rect().centery
+    textpos.centerx = board.background.get_rect().centerx
+    textpos.centery = board.background.get_rect().centery
 
-    background.blit(text, textpos)
-    screen.blit(background, (0, 0)) 
-    pygame.display.update()
+    board.background.blit(text, textpos)
+    board.screen.blit(board.background, (0, 0)) 
+    
+    board.draw_board()
+    return board
 
-    return (screen, background)
-
-def loop(game):
+def main(board):
     spim.stdout.readline()
     spim.stdout.readline()
     spim.stdout.readline()
@@ -54,10 +101,10 @@ def loop(game):
             print 'Found data: %s' % data
             spim.stdout.flush()
             data = data[:-1]
-            game = update_number(game, data)
+            board = update_number(board, data)
         else:
             for event in pygame.event.get():
-                if event.type == QUIT:
+                if event.type == pygame.QUIT:
                     print event
                     return
                 elif event.type == pygame.KEYDOWN:
@@ -67,5 +114,6 @@ def loop(game):
                         spim.stdin.write('0\n')
 
 if __name__ == '__main__':
-    game = setup_pygame()
-    loop(game)
+    pygame.init()
+    init_board = GameBoard(width=132, height=132)
+    main(init_board)
